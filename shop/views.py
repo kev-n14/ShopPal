b0fc631a20a925dce26442bd56bf2f5a3b50825b
kev-n14 <kevinfinn688@gmail.com>
2023-03-25 14:40:22 +0000

    add shop_list.html, display basic table list of items

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+
+def get_shop_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'shop/shop_list.html', context)
--- shoppal/urls.py
+++ shoppal/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shop.views import get_shop_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_shop_list, name='get_shop_list')
 ]
